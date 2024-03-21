d42aec424714f3eada334657c1569caa8590dc79
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-03-21 05:04:38 +0000

    write get initials function

---
+++ src/utils/functions.ts
@@ -0,0 +1,21 @@
+import _ from "lodash"
+
+export function getInitials(fullName: string): string {
+    if (typeof fullName !== "string") {
+        return "NA"
+    }
+
+    const names = fullName.split(' ');
+
+    if (names.length === 1) {
+        // If there is only one name, use the first two letters
+        return _.toUpper(_.take(names[0], 2).join(''));
+    } else if (names.length === 2) {
+        // If there are two names, use the first letter of each name
+        return _.toUpper(_.map(names, name => name[0]).join(''));
+    } else {
+        // If there are three or more names, use the first letter of the first word
+        // and the first letter of the last word
+        return _.toUpper(`${names[0][0]}${names[names.length - 1][0]}`);
+    }
+}
